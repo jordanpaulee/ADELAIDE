572e79027c31db0ad95cf0ef92da80b35299adfe
Jordan Lee <jplee01@wm.edu>
2024-09-15 22:20:11 -0400

    ADELAIDE V0.1

--- brain.py
+++ brain.py
@@ -4,6 +4,7 @@ from elevenlabs import stream
 import ollama
 import constants # import file for API keys
 
+
 # Defining Adelaide
 class adelaide:
     def __init__(self) -> None:
@@ -48,6 +49,7 @@ class adelaide:
 
 
     def start_transcription(self):
+        print(f"\nReal-time transcription: ", end="\r\n")
         self.transcriber = aai.RealtimeTranscriber(
             sample_rate=16_000,
             on_data=self.on_data,
@@ -59,7 +61,9 @@ class adelaide:
         self.transcriber.connect()
 
         microphone_stream = aai.extras.MicrophoneStream(sample_rate=16_000)
+        print(f"\nI'm listening", end="\r\n")
         self.transcriber.stream(microphone_stream)
+        #print(f"\nTranscriber Connected", end="\r\n")
 
     def close_transcription(self):
         if self.transcriber:
@@ -68,6 +72,7 @@ class adelaide:
 
 # Hand-off transcript to Llama 3
     def generate_ai_response(self, transcript):
+        print(f"\nGenerating AI Response...", end="\r\n")
         self.close_transcription()
 
         self.full_transcript.append({"role":"user", "content":transcript.text})
@@ -84,7 +89,7 @@ class adelaide:
         text_buffer = ""
         full_text = ""
         for chunk in ollama_stream:
-            text_buffer += chunk['messages']['content']
+            text_buffer += chunk['message']['content']
             if text_buffer.endswith('.'):
                 audio_stream = self.client.generate(text=text_buffer,
                                                     model="eleven_turbo_v2",
